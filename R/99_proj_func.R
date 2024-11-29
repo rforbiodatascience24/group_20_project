918200ad4fc542cb097ad62129f8da3cd5a34be2
aa <antoine.andreoletti@polytechnique.edu>
2024-11-29 22:47:58 +0100

    Tidyed everything

--- R/99_proj_func.R
+++ R/99_proj_func.R
@@ -1,17 +1,6 @@
-# Function to save the pheatmap (https://stackoverflow.com/questions/43051525/how-to-draw-pheatmap-plot-to-screen-and-also-save-to-file)
-save_pheatmap_pdf <- function(x, filename, width=7, height=7) {
-  stopifnot(!missing(x))
-  stopifnot(!missing(filename))
-  pdf(filename, width=width, height=height)
-  grid::grid.newpage()
-  grid::grid.draw(x$gtable)
-  dev.off()
-}
-
-
 plot_volcano <- function(x, title, fc_cutoff, p_cutoff){
   
-  ## Make sur title input can be used in filepath (using regular expression)
+  ## Make sure title input can be used in filepath
   title <- gsub("[^A-Za-z0-9_\\-]", "_", title)
   
   ## Data wrangling
@@ -25,22 +14,24 @@ plot_volcano <- function(x, title, fc_cutoff, p_cutoff){
     dplyr::mutate(gene_type = case_when(log2FoldChange >= fc_cutoff & padj <= 0.05 ~ "Up regulated",
                                         log2FoldChange <= -fc_cutoff & padj <= 0.05 ~ "Down regulated",
                                         TRUE ~ "Non significant")) 
-  cols <- c("Up regulated" = "gold", "Down regulated" = "blue", "Non significant" = "darkgrey")
+  cols <- c("Up regulated" = "gold", 
+            "Down regulated" = "blue", 
+            "Non significant" = "darkgrey")
   
   ## Plot
   p <- volcano_data |> 
     ggplot(aes(x = log2FoldChange, 
                y = -log10(padj), 
                col=gene_type)) + 
-      geom_point() +
-      geom_hline(yintercept = -log10(p_cutoff), linetype = "dashed") + 
-      geom_vline(xintercept = c(-fc_cutoff, fc_cutoff),
-               linetype = "dashed") +
-      scale_color_manual(values=cols)+
-      labs(
-        title= str_c("Volcano plot - log10(Adj-P-Value) - ", title),
-        colour = "Differencial expression"
-      )
+    geom_point() +
+    geom_hline(yintercept = -log10(p_cutoff), linetype = "dashed") + 
+    geom_vline(xintercept = c(-fc_cutoff, fc_cutoff),
+             linetype = "dashed") +
+    scale_color_manual(values=cols)+
+    labs(
+      title= str_c("Volcano plot - ", title),
+      colour = "Differencial expression"
+    )
   
   ## Create the folder to save it in
   results_dir <- "../results/DEA"
@@ -52,7 +43,7 @@ plot_volcano <- function(x, title, fc_cutoff, p_cutoff){
   ggsave(plot = p, file.path(str_c("../results/DEA/DEA-", 
                                    title, 
                                    ".png")))
-  print(p)
+  p
 }
 
 compute_GSEA <- function(ranked_genes, gene_sets) {
